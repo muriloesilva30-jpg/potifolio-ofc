a13458f3a241ed543bf614b052499023dc515863
Murilo <murilo.e.silva30@escola.pr.gov.br>
2026-09-22 08:47:49 -0300

    Add files via upload

---
+++ atividade5/script.js
@@ -0,0 +1,19 @@
+function maior(){
+    let numero1;
+    let numero2;
+    let Maior;
+
+    numero1 = Number(prompt("digite o primeiro número"));
+    numero2 = Number(prompt("digite o segundo número"));
+
+    
+
+if(numero > 2 == 0){
+    alert("O número é Maior");
+}
+
+    Maior = numero1  numero2
+
+    alert("O reultado é: \n Maior: " + Maior );
+
+}
